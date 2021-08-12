9072774b11859059ec63321d18aaa48a00c40559
Benjamin Williamson <badw@dhcp-10-24-105-188.wlan.ntnu.no>
2021-08-12 12:27:16 +0200

    Added Files

---
+++ arcs/analysis_functions.py
@@ -0,0 +1,257 @@
+import random
+from chempy.equilibria import Equilibrium,EqSystem
+from chempy import Substance
+import copy
+import networkx as nx
+import itertools as it
+from tqdm import tqdm
+from collections import defaultdict
+
+
+import os
+import warnings
+
+from multiprocessing import Process, Queue
+from datetime import datetime
+import random
+import math
+import threading
+import multiprocessing
+import numpy as np
+import pandas as pd 
+from pathos.helpers import mp as pmp
+
+
+class Traversal:
+    def __init__(self,graph,reactions,concs,**kwargs):
+        self.graph = graph
+        self.reactions = reactions
+        self.concs = copy.deepcopy(concs)
+        self.progress = kwargs['progress']
+        
+    
+    def random_walk(self,T,P,depth=10):
+        nodes = [n for n in self.graph[T][P].nodes() if isinstance(n,str)]
+        available = [a for a,i in self.concs.items() if i !=0]
+        source = random.choice([n for n in nodes if n in available])
+        path = []
+        for i in range(depth):
+            target = random.choice(nodes)
+            p = nx.shortest_path(self.graph[T][P],source,target)
+            source = target
+            if not len(p) == 1:
+                path.append(p)
+        return(path)
+    
+    def generate_eqsystems_from_path(self,path,T,P):
+        charged_species = {'CO3H':-1,'NH4':+1,'NH2CO2':-1}
+        r_i = [i for i in path if isinstance(i,int)]
+        rs = [self.reactions[T][P][i] for i in r_i]
+        eqsystems = []
+        for i in rs:
+            r = i['e'].reac
+            p = i['e'].prod
+            k = i['k']
+            substances = {}
+            for n in list(it.chain(*[list(r)+list(p)])):
+                if n in list(charged_species.keys()):
+                    s = Substance.from_formula(n,charge=charged_species[n])
+                    substances[s.name] = s
+                    #substances[s.charge] = charged_species[n]
+                else:
+                    s = Substance.from_formula(n,charge=0)
+                    substances[s.name] = s 
+                    #substances[s.charge] = 0 
+            #substances = {s.name:s for s in [Substance.from_formula(n) 
+            #                                     for n in list(it.chain(*[list(r) + list(p)]))]}
+            try:
+                eqsystems.append(EqSystem([Equilibrium(r,p,k)],substances))
+            except:
+                eqsystems.append([])
+        return(eqsystems[0])
+    
+    def equilibrium_concentrations_from_walk(self,eqwalk):
+        fc = copy.deepcopy(self.concs)
+        concs = {0:copy.deepcopy(fc)}
+        successful_equations = []
+        for i,eqsys in enumerate(eqwalk):
+            fc = copy.deepcopy(fc)
+            try:
+                x,sol,sane = eqsys.root(fc)
+                assert sol['success'] and sane
+                for n,c in enumerate(x):
+                    fc[eqsys.substance_names()[n]] = c
+                    
+                concs[i+1] = fc
+                successful_equations.append(eqsys)
+            except:
+                concs[i+1] = fc
+        return(concs,successful_equations)
+    
+    def sample_graph(self,T,P,sample_length=100,path_depth=100):
+        total_data = {0:self.concs}
+        s_equs = []
+        with tqdm(total=sample_length*path_depth,disable=self.progress) as pbar:
+            for i in range(1,sample_length,1):
+                p = self.random_walk(T,P,path_depth)
+                walk = [] 
+                for path in p:
+                    walk.append(self.generate_eqsystems_from_path(path,T,P))
+                    pbar.update(1)    
+                da,eq = self.equilibrium_concentrations_from_walk(walk)
+                total_data[i] = da[len(da)-1]
+                s_equs.append(eq)
+        return(total_data,s_equs)
+    
+    def sample_graph_mp(self,T,P,sample_length=100,path_depth=100,nprocs=4):
+        
+        def mp_function(samples,out_q):
+            data = {}
+            for sample in samples:
+                p = self.random_walk(T,P,path_depth)
+                walk = []
+                for path in p:
+                    walk.append(self.generate_eqsystems_from_path(path,T,P))
+                da,eq = self.equilibrium_concentrations_from_walk(walk)
+                data[sample] = {'data':da[len(da)-1],'equation_statistics':eq}
+            out_q.put(data)
+            
+            
+        resultdict = {0:{'data':self.concs,'equation_statistics':[]}}
+        manager = pmp.Manager()
+        queue = manager.Queue()
+        total_samples = range(1,sample_length,1)
+        chunksize = int(math.ceil(len(total_samples)/float(nprocs)))
+        procs = []
+        for i in range(nprocs):
+            p = pmp.Process(target=mp_function,
+                        args=(total_samples[chunksize*i:chunksize*(i+1)],queue))
+            procs.append(p)
+            p.start() 
+    
+        for i in range(nprocs):
+            resultdict.update(queue.get(timeout=1800))
+            
+            
+        for p in procs:
+            p.join()
+            
+        queue.close()
+        queue.join_thread()
+            
+            
+        return(resultdict)
+    
+def get_reaction_statistics(t_and_p_data):
+    trange = list(t_and_p_data.keys()) #test
+    prange = list(t_and_p_data[trange[0]].keys())
+    equations = {}
+    for T in trange:
+        eqs_p  = {}
+        for P in prange:
+            eqs = []
+            for x in t_and_p_data[T][P].keys():
+                if t_and_p_data[T][P][x]['equation_statistics']:
+                    eqs.append(t_and_p_data[T][P][x]['equation_statistics'])
+            eqs_p[P] = eqs
+        equations[T] = eqs_p
+    
+    def get_dataframes(list_of_equations):
+        appearances = defaultdict(int)
+        for sample in list_of_equations:
+            for i in sample:
+                appearances[i.string()] += 1
+    
+        equation_statistics = {}
+        for equation,frequency in appearances.items():
+            eq,k = equation.split(';')
+            equation_statistics[eq] = {'k':k.split('\n')[0],'frequency':frequency}
+        d = pd.DataFrame(equation_statistics).T.sort_values(by='frequency',ascending=False)
+        return(d)
+
+    dict_of_dataframes = {T:{P:get_dataframes(equations[T][P]) 
+                             for P in prange} 
+                          for T in trange}
+    return(dict_of_dataframes)
+
+def get_mean_change_in_data(t_and_p_data):
+    data = {T:
+            {P:
+              {x:t_and_p_data[T][P][x]['data']
+               for x in t_and_p_data[T][P].keys()} 
+             for P in t_and_p_data[T].keys()} 
+            for T in t_and_p_data.keys()}
+    d = pd.DataFrame(data) 
+    mean_dataframe = pd.DataFrame({T:
+                                   {P:pd.DataFrame(d[T][P]).T.mean().drop('CO2') - pd.DataFrame(d[T][P])[0].drop('CO2')
+                                    for P in d.index} 
+                                   for T in d.columns})
+    return(mean_dataframe)
+    
+
+class PrettyPlot:
+    
+    def __init__(self,graph,concs,path,index,directory):
+        self.graph = graph
+        self.concs = concs
+        self.path = path
+        self.index = index
+        self.directory = directory
+        
+        
+    def make_options(self,p):
+        node_sizes = [1 if isinstance(n,str) else 0 for n in self.graph.nodes()]
+        node_colours = []
+        alphas = []
+        for n in self.graph.nodes:
+             if isinstance(n,str):
+                    if n in list(self.concs.keys()):
+                        node_colours.append((0.8,0.0,0.0))
+                        alphas.append(1.0)
+                    else:
+                        node_colours.append((0.6,0.4,0.9))
+                        alphas.append(0.5)
+             else:
+                 node_colours.append((0.0,0.4,0.8))
+                 alphas.append(0.2)
+            
+        node_options = {'node_color': node_colours,
+                   'alpha': alphas,
+                   'node_size': node_sizes}
+        
+        edge_colours = []
+        for e in self.graph.edges:
+            if p[0] == e[0] and p[1] == e[1]:
+                edge_colours.append((0.9,0.0,0.0,1.0))
+            else:
+                edge_colours.append((0.2,np.random.random(),np.random.random(),0.1))
+        
+        edge_options = {'connectionstyle':'arc3,rad=0.9',
+                        'width':1,
+                       'edge_color':edge_colours}
+        
+        return([node_options,edge_options])
+    
+    def make_directory(self):
+        np = os.path.join(self.directory,str(self.index))
+        try:
+            os.mkdir(np)  
+        except:
+            pass
+        return(np)
+        
+    def plot_graph(self):
+        np = self.make_directory()
+        for i,pt in enumerate(self.path):
+            node_options,edge_options = self.make_options(pt)        
+            fig,ax = plt.subplots(figsize=(20,20),dpi=100)
+            pos = nx.kamada_kawai_layout(self.graph)
+            n = nx.draw_networkx_nodes(self.graph,pos,ax=ax,
+                                       node_color=node_options['node_color'],
+                                       alpha=node_options['alpha'],
+                                      node_size=node_options['node_size'])
+            e = nx.draw_networkx_edges(self.graph,pos,ax=ax,**edge_options)
+            ax.set_facecolor((0.1, 0.1, 0.1))
+            f = os.path.join(np,'nl_{}_{}.png'.format(self.index,i))
+            plt.savefig(f,transparent=False)        
+            
---
+++ arcs/setup_functions.py
@@ -0,0 +1,327 @@
+import os,gzip
+from ase.io import read,write
+from pymatgen.io.ase import AseAtomsAdaptor
+from pymatgen.symmetry.analyzer import PointGroupAnalyzer
+from ase.thermochemistry import IdealGasThermo
+from scipy.constants import Boltzmann, e
+import numpy as np 
+import itertools as it
+import numpy as np 
+from chempy import balance_stoichiometry
+from chempy import Reaction
+from chempy.equilibria import Equilibrium
+from tqdm import tqdm
+import networkx as nx
+
+def get_compound_directory(base,compound,size):
+    return(os.path.join(base,compound,size))
+
+class GetEnergyandVibrations:
+    '''Class to get the Total Energy and Vibrations from a directory containing a calculations'''
+    def __init__(self,directory):
+        self.directory = directory
+        
+    def atoms(self):
+        structure = read('{}/POSCAR.gz'.format(self.directory))
+        return(structure)
+        
+    def energy(self):
+        outcar = gzip.open('{}/OUTCAR.gz'.format(self.directory),'tr').readlines()
+        for line in outcar:
+            if 'y=' in line:
+                energy = float(line.split()[-1])
+        if len(list(dict.fromkeys(self.atoms().get_atomic_numbers()))) == 1:
+            energy = energy / self.atoms().get_global_number_of_atoms()
+    
+        return(energy)
+    
+    def spin(self):
+        outcar = gzip.open('{}/OUTCAR.gz'.format(self.directory),'tr').readlines()
+        for line in outcar:
+            if 'NELECT' in line:
+                nelect = float(line.split()[2])
+                return([0 if nelect %2 == 0 else 1][0])
+            
+    def pointgroup(self):
+        atoms = self.atoms()
+        pg = PointGroupAnalyzer(AseAtomsAdaptor.get_molecule(atoms)).get_pointgroup()
+        return(pg.sch_symbol)
+    
+    def islinear(self):
+        num_at = self.atoms()
+        if num_at.get_global_number_of_atoms() == 1:
+            return('monatomic')
+        else:
+            pg = self.pointgroup()
+            if '*' in pg:
+                return('linear')
+            else:
+                return('nonlinear')
+
+    def rotation_num(self):
+        pg = [x for x in self.pointgroup()]
+        if pg[0] == 'C':
+            if pg[-1] == 'i':
+                rot = 1
+            elif pg[-1] == 's':
+                rot = 1
+            elif pg[-1] == 'h':
+                rot = int(pg[1])
+            elif pg[-1] == 'v':
+                if pg[1] == '*':
+                    rot = 1
+                else:
+                    rot = int(pg[1])
+            elif len(pg) == 2:
+                rot = int(pg[-1])
+                
+        elif pg[0] == 'D':
+            if pg[-1] == 'h':
+                if pg[1] == '*':
+                    rot = 2
+                else:
+                    rot = 2*int(pg[1])
+            elif pg[-1] == 'd':
+                rot = 2*int(pg[1])
+            elif len(pg) == 2:
+                rot = 2*int(pg[1])
+            
+        elif pg[0] == 'T':
+            rot = 12
+        
+        elif pg[0] == 'O':
+            rot = 24
+        
+        elif pg[0] == 'I':
+            rot = 60        
+                    
+        return(rot)          
+        
+    def vibrations(self):
+        new_directory = os.path.join(self.directory,'ibrion_6')
+        outcar = gzip.open('{}/OUTCAR.gz'.format(new_directory),'tr').readlines()
+        frequencies = []
+        for line in outcar:
+            if 'THz' in line:
+                if not 'f/i' in line: # we ignore imaginary modes
+                    ev = float(line.split()[-2]) / 1000
+                    frequencies.append(ev)
+        return(frequencies)      
+    
+    def as_dict(self):
+        return({'atoms':self.atoms(),
+                'spin':self.spin(),
+                'rotation_num':self.rotation_num(),
+                'islinear':self.islinear(),
+                'energy':self.energy(),
+                'vibrations':self.vibrations()})
+    
+    
+class ReactionGibbsandEquilibrium:
+    
+    def __init__(self,reaction,temperature,pressure,reaction_input):
+        self.reaction = reaction
+        self.temperature = temperature
+        self.pressure = pressure*100000 #pressure in bar
+        self.reaction_input = reaction_input
+        
+    def Gibbs(self,c):
+        data = self.reaction_input[c]
+        igt = IdealGasThermo(vib_energies=data.vibrations(),
+                                        geometry=data.islinear(),
+                                        potentialenergy=data.energy(),
+                                        atoms=data.atoms(),
+                                        symmetrynumber=data.rotation_num(),
+                                        spin=data.spin(),
+                                        natoms=data.atoms().get_global_number_of_atoms())
+        G = igt.get_gibbs_energy(self.temperature,self.pressure,verbose=False)
+        H = igt.get_enthalpy(self.temperature,verbose=False)
+        S = igt.get_entropy(self.temperature,self.pressure,verbose=False) 
+        Z = igt.get_entropy(self.temperature,self.pressure,verbose=False)
+        return({'G':G,'H':H,'S':S,'Z':Z})
+    
+    def reaction_energy(self):
+        prod = self.reaction.prod
+        reac = self.reaction.reac
+        reaction_compounds = list(prod)+list(reac)
+        # need to add a charge neutrality condition and mass balance violation
+        gibbs = {c:self.Gibbs(c) for c in reaction_compounds}
+        prod_sum = np.sum([self.Gibbs(c)['G']*prod[c] for c in gibbs if c in prod])
+        reac_sum = np.sum([self.Gibbs(c)['G']*reac[c] for c in gibbs if c in reac])
+        return(float(prod_sum - reac_sum))
+    
+    def equilibrium_constant(self):
+        K = np.exp(-(self.reaction_energy()*e)/(Boltzmann*self.temperature))
+        return(K)
+    
+    def as_dict(self):
+        return({'G_react':self.reaction_energy(),'K_react':self.equilibrium_constant()})
+
+
+class ReactionsGenerator:
+    ''' once you have used this it is highly recommended that you  run class RemoveDuplicateReactions'''
+    
+    def __init__(self,compounds,max_length):
+        self.compounds = compounds
+        self.max_length = max_length
+    
+
+    def get_combinations(self,length):
+        combinations = []
+        init = list(it.permutations(self.compounds,length))
+        for i in range(1,length):
+            for j in init:
+                combinations.append([j[0:i],j[i:len(j)]])
+        return(combinations)
+    
+    def screen_combinations(self):
+        screened = []
+        for l in range(2,self.max_length+1):
+            for poss in tqdm(self.get_combinations(l)):
+                r,p = poss
+                try:
+                    screened.append(balance_stoichiometry(list(r),list(p)))
+                except:
+                    pass
+        return(screened)
+       
+    def get_reactions(self):
+        reactions = []
+        for i in self.screen_combinations():
+            r,p = i
+            try:
+                substances = list(r) + list(p)
+                reactions.append(Equilibria(r,p))  
+            except:
+                pass
+        return(reactions)
+    
+class RemoveDuplicateReactions: # this needs checking
+    def __init__(self,reactions,divisor):
+        self.reactions = reactions
+        self.divisor = divisor
+    
+    def check_same(self,i,j):
+        r1 = list(self.reactions[i].reac)
+        p1 = list(self.reactions[i].prod)
+    
+        r2 = list(self.reactions[j].reac)
+        p2 = list(self.reactions[j].prod)
+    
+        action = 0
+    
+        if all(x in r2 for x in r1):
+            if all(y in p2 for y in p1):
+                action = 1
+        elif all(x in r2 for x in p1):
+            if all(y in p2 for y in r1):
+                action = 1
+        
+        return(action)
+
+    def run_through_permutations(self,l):
+        for i,j in l:
+            act = self.check_same(i,j)
+            if act == 1:
+                self.reactions.pop(j)
+                return(0)
+                break
+            
+    def iterate(self):
+        value = 0
+        while value == 0:
+            l = list(it.combinations(list(range(len(self.reactions))),2))
+            value = self.run_through_permutations(l)
+        return(self.reactions)
+    
+    def clean(self,split):
+        split_reactions = np.array_split(list(it.chain(*[self.reactions])),split)
+        cleaned = []
+        for r_split in split_reactions:
+            self.reactions = r_split.tolist()
+            d = self.iterate()
+            cleaned.append(d)
+        return(cleaned)
+
+    def whileclean(self):
+        for i in tqdm(range(self.divisor,0,-1)):        
+            finished = False
+            while finished == False:
+                init = len(list(it.chain(*[self.reactions])))
+                self.reactions = self.clean(i)
+                final = len(list(it.chain(*self.reactions)))
+                if init == final:
+                    finished = True
+        return(self.reactions)
+
+class ApplyDataToReaction:
+    
+    def __init__(self,trange,prange,reactions,compound_data):
+        self.trange = trange
+        self.prange = prange
+        self.reactions = reactions
+        self.compound_data = compound_data
+        
+    def get_t_p_data(self,t,p):
+        reactions = {i:{'e':r,'k':ReactionGibbsandEquilibrium(r,t,p,self.compound_data).equilibrium_constant()} 
+                     for i,r in tqdm(enumerate(self.reactions))}
+        return(reactions)
+        
+    def as_dict(self):
+        data = {}
+        #with tqdm(len(self.trange)*len(self.prange)*len(self.reactions)) as pbar:
+        for t in self.trange:
+                data[t] = {p:self.get_t_p_data(t,p) for p in self.prange}
+        return(data) 
+
+        
+class GraphGenerator:
+    
+    def __init__(self,preloaded_data):
+        self.preloaded_data = preloaded_data
+    
+    def multidigraph(self,T,P):
+        t = nx.MultiDiGraph(directed=True)
+        for i,reac in self.preloaded_data[T][P].items():
+            r = list(reac['e'].reac)
+            p = list(reac['e'].prod)
+            k = reac['k'] # maybe check equilibrium.as_reactions ( gives forward and backward reactions!)
+            if k <= 1: #favours reactants
+                t.add_weighted_edges_from([c,i,1/k] for c in r)
+                t.add_weighted_edges_from([i,c,k] for c in r)
+                t.add_weighted_edges_from([i,c,1/k] for c in p)
+                t.add_weighted_edges_from([c,i,k] for c in p)
+            elif k >= 1: #favours products
+                t.add_weighted_edges_from([c,i,1/k] for c in r)
+                t.add_weighted_edges_from([i,c,k] for c in r)
+                t.add_weighted_edges_from([i,c,1/k] for c in p)
+                t.add_weighted_edges_from([c,i,k] for c in p)
+        return(t) #need to check shortest pathways
+    
+    def multigraph(self,T,P):
+        t = nx.MultiGraph()
+        for i,reac in self.preloaded_data[T][{}].items():
+            r = list(reac['e'].reac)
+            p = list(reac['e'].prod)
+            t.add_edges_from([c,i] for c in r)
+            t.add_edges_from([i,c] for c in r)
+            t.add_edges_from([c,i] for c in p)
+            t.add_edges_from([i,c] for c in p)
+        return(t)
+    
+    def multidigraph_from_t_and_p_range(self,trange,prange):
+        graphs = {}
+        with tqdm(total=len(trange)*len(prange)) as pbar:
+            for T in trange:
+                pdict = {}
+                for P in prange:
+                    pdict[P] = self.multidigraph(T,P)
+                    pbar.update(1)
+                graphs[T] = pdict
+        return(graphs)
+                
+
+        
+        
+        
+          
