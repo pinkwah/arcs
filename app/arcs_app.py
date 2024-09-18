ea745ee247de7ed252d704c27af208c50e10c222
Olve Maudal <olvm@equinor.com>
2024-09-18 07:44:02 +0200

    Revert "Use the large datasets if they exists"
    This reverts commit 55ad5bd66f62a4c165263660c7b73f2271908d4e.

--- app/arcs_app.py
+++ app/arcs_app.py
@@ -12,10 +12,7 @@ def start():
     port = int(os.getenv("PORT", "8050"))
     host = os.getenv("HOST", "127.0.0.1")
     this_dir, this_filename = os.path.split(__file__)
-    file_location = os.path.join(os.path.dirname(__file__),'data')
-    large_dataset = os.path.join(os.path.dirname(__file__),'data/large_dataset/')
-    if os.path.exists(large_dataset):
-        file_location = large_dataset
+    file_location = os.path.join(os.path.dirname(__file__),'data/')
     server_is_started = Condition()
 
     # Set the process title.
