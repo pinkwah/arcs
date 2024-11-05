ed1c0695af1bce979746540947c7edce03cd139e
Zohar Malamant <ZOM@equinor.com>
2024-11-05 12:13:01 +0100

    Make FastAPI test faster

--- api/app.py
+++ api/app.py
@@ -1,48 +1,82 @@
-import os
+from __future__ import annotations
+from contextlib import asynccontextmanager
+from pathlib import Path
+from typing import TypedDict
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import pickle
 from arcs.traversal import Traversal
+import chempy
+import networkx as nx
 
-app = FastAPI()
+
+class _ReactionDict(TypedDict):
+    e: chempy.Equilibrium
+    k: float
+    g: float
+
+
+DATA_DIR = Path(__file__).parent.parent / "arcs/dash_app/data"
+REACTIONS: dict[float, dict[float, dict[int, _ReactionDict]]] = {}
+GRAPHS: dict[float, dict[float, nx.MultiDiGraph]] = {}
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global REACTIONS
+    global GRAPH
+
+    with open(DATA_DIR / "SCAN_reactions.p", "rb") as stream:
+        REACTIONS = pickle.load(stream)
+    with open(DATA_DIR / "SCAN_graph.p", "rb") as stream:
+        GRAPH = pickle.load(stream)
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 class SimulationRequest(BaseModel):
-    trange: list = [300]
-    prange: list = [10]
-    concs: dict = {}
-    settings: dict = {}
+    temperature: float
+    pressure: float
+    concs: dict[str, float] = Field(default_factory=dict)
+    samples: int
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "temperature": 300,
+                    "pressure": 10,
+                    "concs": {
+                        "SO2": 10e-6,
+                        "NO2": 50e-6,
+                        "H2S": 30e-6,
+                        "H2O": 20e-6,
+                    },
+                    "samples": 10,
+                }
+            ]
+        }
+    }
 
 
 @app.post("/run_simulation")
-async def run_simulation(request: SimulationRequest):
-    trange = request.trange
-    prange = request.prange
-    concs = request.concs
-    settings = request.settings
+async def run_simulation(form: SimulationRequest):
+    global GRAPH
+    global REACTIONS
 
-    graph = pickle.load(
-        open(
-            os.path.join(
-                os.path.dirname(__file__), "../arcs/dash_app/data/SCAN_graph.p"
-            ),
-            "rb",
-        )
-    )
     traversal = Traversal(
-        graph=graph,
-        reactions=os.path.join(
-            os.path.dirname(__file__), "../arcs/dash_app/data/SCAN_reactions.p"
-        ),
+        graph=GRAPH,
+        reactions=REACTIONS,
     )
-    traversal.run(trange=trange, prange=prange, save=False, ic=concs, **settings)
+    traversal.run(
+        trange=[form.temperature],
+        prange=[form.pressure],
+        ic=form.concs,
+        sample_length=10,
+    )
 
     results = {"initfinaldiff": traversal.initfinaldiff, "data": traversal.data}
-    # TODO: return graph data?
     return results
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="127.0.0.1", port=8000)
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,14 +1,20 @@
-# test_app.py
+from __future__ import annotations
+from typing import Iterable
+import pytest
 from fastapi.testclient import TestClient
 from api.app import app
 
-client = TestClient(app)
+
+@pytest.fixture
+def client() -> Iterable[TestClient]:
+    with TestClient(app) as client:
+        yield client
 
 
-def test_run_simulation():
+def test_run_simulation(client):
     payload = {
-        "trange": [300],
-        "prange": [10],
+        "temperature": 300,
+        "pressure": 10,
         "concs": {
             "CO2": 1,
             "H2O": 2e-05,
@@ -16,17 +22,7 @@ def test_run_simulation():
             "SO2": 1e-05,
             "NO2": 5e-05,
         },
-        "settings": {
-            "nprocs": 1,
-            "sample_length": 320,
-            "max_rank": 10,
-            "max_compounds": 5,
-            "probability_threshold": 0.1,
-            "path_depth": 5,
-            "ceiling": 2000,
-            "scale_highest": 0.2,
-            "rank_small_reactions_higher": True,
-        },
+        "samples": 10,
     }
     response = client.post("/run_simulation", json=payload)
     assert response.status_code == 200
